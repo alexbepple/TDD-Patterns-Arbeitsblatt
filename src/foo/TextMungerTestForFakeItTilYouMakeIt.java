98fae99404b4152bc62e4145baa6e60efefd16c7
Alex Bepple <alex@bepple.de>
2011-12-13 16:05:01 +0100

    Ausgangssituation für TDD-Arbeitsblätter

---
+++ src/foo/TextMungerTestForFakeItTilYouMakeIt.java
@@ -0,0 +1,27 @@
+package foo;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class TextMungerTestForFakeItTilYouMakeIt {
+	
+	@Test
+	public void laesstWortBisDreiZeichenUnveraendert() throws Exception {
+		assertThat(verdrehe("abc"), is("abc"));
+	}
+
+	@Test
+	public void drehtZeichenInDerMitteUmFuerWortAusVierZeichen() throws Exception {
+		assertThat(verdrehe("abcd"), is("acbd"));
+	}
+	
+	private String verdrehe(String text) {
+		if ("abcd".equals(text)) {
+			return "acbd";
+		}
+		return text;
+	}
+
+}
---
+++ src/foo/TextMungerTestForTriangulate.java
@@ -0,0 +1,31 @@
+package foo;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class TextMungerTestForTriangulate {
+	
+	@Test
+	public void laesstWortBisDreiZeichenUnveraendert() throws Exception {
+		assertThat(verdrehe("abc"), is("abc"));
+	}
+
+	@Test
+	public void drehtZeichenInDerMitteUmFuerWortAusVierZeichen() throws Exception {
+		assertThat(verdrehe("abcd"), is("acbd"));
+	}
+	
+	private String verdrehe(String text) {
+		if ("abcd".equals(text)) {
+			return "" + 
+				text.charAt(0) + 
+				text.charAt(2) + 
+				text.charAt(1) + 
+				text.charAt(3);
+		}
+		return text;
+	}
+
+}
